eddea084e6a1ac982419bc3b779b54b133789805
Joseph Corona <josed.hc96@gmail.com>
2018-11-05 02:34:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,16 +16,16 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
-  def move(board, index, current_player)
-    board[index] = current_player
+  def move(index, current_player)
+    @board[index] = current_player
   end
 
-  def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
+  def position_taken?(location)
+  @board[location] != " " && board[location] != ""
   end
 
-  def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+  def valid_move?(index)
+  index.between?(0,8) && !position_taken?(index)
   end
 
   def current_player
@@ -40,15 +40,15 @@ class TicTacToe
   user_input.strip.to_i - 1
   end
 
-  def turn(board)
+  def turn
     puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
-    if valid_move?(board, index)
-      move(board, index, current_player(board))
-      display_board(board)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
     else
-      turn(board)
+      turn
     end
   end
 
